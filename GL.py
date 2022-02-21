2f6f5bea4d0d0db9a98cd10b17498c70c24a5213
Antonio RH <arodh91@gmail.com>
2022-02-21 21:52:00 +0000

    Computes pi and error. Wrong error per iteration

--- GL.py
+++ GL.py
@@ -1,30 +1,66 @@
 #!/usr/bin/env python3
 import numpy as np
+import gmpy2
 from decimal import Decimal
-def GL(nMax):
-    a = Decimal(1.0);
-    b = Decimal(1.0/np.sqrt(2));
-    s = Decimal(1/4.0);
-
-    for i in range(nMax-1):
-        aNew = arMean(a,b);
-        c = a - aNew;
-       # print(a**2/s, aNew**2/s)
+gmpy2.local_context(gmpy2.context(), precision=100);
+def GL(nMax, precision, verbose = 0):
+    
+    with gmpy2.local_context(gmpy2.context(), precision=100) as ctx:
+        ctx.precision += precision
+        a = gmpy2.mpfr(float('1.0'));
+        b = gmpy2.mpfr(float(1.0/gmpy2.sqrt(2)));
+        s = gmpy2.mpfr(float(1/4.0));
         
-        bNew = geoMean(a,b);
-        s = s - 2**i*c**2;
+        
 
-        b = bNew;
-        a = aNew;
-    return aNew**2/s
+        for i in range(nMax ):
+            out, error = computeOutput(a, s);        
+            aNew = arMean(a,b);
+            c = sub(a,aNew);
+
+            if verbose:
+               print(a**2/s, aNew**2/s)
+            
+            
+            bNew = geoMean(a,b);
+            out, error = computeOutput(a, s);
+
+            s = sub(s, 2**i*c**2);            
+            b = bNew;
+            a = aNew;        
+        
+        return out, error
+
+
+def computeOutput(a,s):
+    out = div(a**2, s);
+    error = sub(out, gmpy2.const_pi());
+    return  out, error
+
+def add(a,b):
+    return gmpy2.add(a,b);
+
+def sub(a,b):
+    return gmpy2.sub(a,b);
+
+def div(a,b):
+    return gmpy2.div(a,b);
+
+def prod(a,b):
+    return gmpy2.mul(a,b);
+
+def sqrt(a):
+    return gmpy2.sqrt(a);
 
 def arMean(a,b):
-    return (a+b)/2;
+    return div(add(a,b),2);
+    
 
 def geoMean(a,b):
-    return np.sqrt(a*b);
+    return sqrt(prod(a, b));
 
-out = GL(10000);
+out, error = GL(1, 57, 1);
 
 #print(f'The value of pi is approximately {out:.64f}. {out-np.pi:.64f}')
-print(out*Decimal(1e6))
+print(out)
+print(error)
