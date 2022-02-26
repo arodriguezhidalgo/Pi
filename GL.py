eb38d7b83f6321f11e5dfe8ddfe9316231b6c0be
Antonio RH <arodh91@gmail.com>
2022-02-26 10:31:03 +0000

    Working as a class

--- GL.py
+++ GL.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
-import numpy as np
+
 import gmpy2
-from decimal import Decimal
-gmpy2.local_context(gmpy2.context(), precision=100);
-def GL(nMax, precision, verbose = 0):
-    
-    with gmpy2.local_context(gmpy2.context(), precision=100) as ctx:
-        ctx.precision += precision
-        a = gmpy2.mpfr('1.0');
-        b = gmpy2.mpfr(1.0/gmpy2.sqrt(2));
-        s = gmpy2.mpfr(1/4.0);
 
-        for i in range(nMax+1 ):
-            out, error = computeOutput(a, s, precision);        
-            aNew = arMean(a,b);
-            c = sub(a,aNew);
+class GL:
+    def __init__(self, prec):
+        self.precision = prec;
+        self.ctx = gmpy2.get_context();
+        self.ctx.precision = prec;
 
-            if verbose:
-               print(prod(a,a)/s, prod(aNew,aNew)/s)           
-            
-            bNew = geoMean(a,b);           
+    def computePi(self, nMax, precision, verbose = 0):        
+            a = gmpy2.mpfr('1.0');
+            b = gmpy2.mpfr(1.0/gmpy2.sqrt(2));
+            s = gmpy2.mpfr(1/4.0);
 
-            s = sub(s, prod(gmpy2.exp2(i),prod(c,c)));            
-            b = bNew;
-            a = aNew;        
-        
-        return out, error
+            for i in range(nMax+1 ):
+                out = self.computeOutput(a, s);        
+                aNew = arMean(a,b);
+                c = sub(a,aNew);
 
+                if verbose:
+                    print(prod(a,a)/s, prod(aNew,aNew)/s)           
+                
+                bNew = geoMean(a,b);           
 
-def computeOutput(a,s, precision):
-    out = div(prod(a,a), s);
-    error = sub(out, gmpy2.const_pi(10000));
-    return  out, error
+                s = sub(s, prod(gmpy2.exp2(i),prod(c,c)));            
+                b = bNew;
+                a = aNew; 
+
+                outLower = self.computeOutput(a, s);       
+                error = out-outLower;
+            return out, error
+    def computeOutput(self, a,s):
+        out = div(prod(a,a), s);
+        return  out
 
 def add(a,b):
     return gmpy2.add(a,b);
@@ -49,14 +50,7 @@ def sqrt(a):
     return gmpy2.sqrt(a);
 
 def arMean(a,b):
-    return div(add(a,b),2);
-    
+    return div(add(a,b),2);    
 
 def geoMean(a,b):
     return sqrt(prod(a, b));
-
-out, error = GL(8, 2000);
-
-#print(f'The value of pi is approximately {out:.64f}. {out-np.pi:.64f}')
-print(out)
-print(error)
---
+++ Main.py
@@ -0,0 +1,5 @@
+from GL import GL
+obj = GL(2000)
+out, error = obj.computePi(2,2000)
+print(out)
+print(error)
